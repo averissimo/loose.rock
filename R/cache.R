0733044d5e26cb9524875897eab9e58d089ce483
André Veríssimo <afsverissimo@gmail.com>
2018-04-09 00:02:36 +0100

    adding more complete run.cache
    runCache had a bug when different functions were called with same arguments then it would save to same file.. depending only on prefix

--- R/cache.R
+++ R/cache.R
@@ -68,6 +68,8 @@ setMethod('runCache',
                    show.message  = TRUE,
                    force.recalc  = FALSE,
                    add.to.hash   = NULL) {
+
+  warning('DEPRECATED, use run.cache instead! if runCache is called with same arguments but different functions it will save to same cache.')
   args <- list(...)
   if (!is.null(seed)) {
     args[['runCache.seed']] <- seed
@@ -99,7 +101,100 @@ setMethod('runCache',
     load(path)
   } else {
     result <- fun(...)
+    if (show.message) {
+      cat(sprintf('Saving in cache: %s\n', path))
+    }
     save(result, file = path)
   }
   return(result)
 })
+
+
+#' Run function and save cache
+#'
+#' This method saves the function that's being called
+#'
+#' @param base.dir directory where data is stored
+#' @param fun function call name
+#' @param ... parameters for function call
+#' @param seed when function call is random, this allows to set seed beforehand
+#' @param cache.prefix prefix for file name to be generated from parameters (...)
+#' @param cache.digest cache of the digest for one or more of the parameters
+#' @param show.message show message that data is being retrieved from cache
+#' @param force.recalc force the recalculation of the values
+#' @param add.to.hash something to add to the filename generation
+#'
+#' @return the result of fun(...)
+#' @export
+#'
+#' @examples
+#' runCache(c, 1, 2, 3, 4)
+#' # next three should use the same cache
+#' runCache(c, 1, 2, 3, 4)
+#' runCache(c, 1, 2, 3, 4, cache.digest = list(digest.cache(1)))
+#' runCache(c, a=1, 2, c=3, 4) # should get result from cache
+setGeneric("run.cache", function(fun,
+                                ...,
+                                seed = NULL,
+                                base.dir = tempdir.cache(),
+                                cache.prefix = 'generic_cache',
+                                cache.digest = list(),
+                                show.message = TRUE,
+                                force.recalc = FALSE,
+                                add.to.hash = NULL) {
+  cat('Wrong arguments, first argument must be a path and second a function!\n')
+  cat('  Usage: run(tmpBaseDir, functionName, 1, 2, 3, 4, 5)\n')
+  cat('  Usage: run(tmpBaseDir, functionName, 1, 2, 3, 4, 5, cache.prefix = \'someFileName\', force.recalc = TRUE)\n')
+})
+
+setMethod('run.cache',
+          signature('function'),
+          function(fun,
+                   ...,
+                   seed          = NULL,
+                   base.dir      = tempdir.cache(),
+                   cache.prefix  = 'generic_cache',
+                   cache.digest = list(),
+                   show.message  = TRUE,
+                   force.recalc  = FALSE,
+                   add.to.hash   = NULL) {
+            args <- list(...)
+            if (!is.null(seed)) {
+              args[['runCache.seed']] <- seed
+              set.seed(seed)
+            }
+            if (!is.null(add.to.hash)) {
+              args[['runCache.add.to.hash']] <- add.to.hash
+            }
+            #
+            args <- lapply(seq_along(args), function(ix) {
+              if (length(cache.digest) >= ix && !is.null(cache.digest[[ix]])) {
+                return(cache.digest[[ix]])
+              }
+              digest.cache(args[[ix]])
+            })
+
+            args[['cache.fun']] <- digest.cache(fun)
+
+            dir.create(base.dir, showWarnings = FALSE)
+            my.digest   <- digest.cache(args)
+            filename    <- sprintf('%s-H_%s.RData', cache.prefix, my.digest)
+            parent.path <- strtrim(my.digest, width = 4)
+            #
+            dir.create(file.path(base.dir, parent.path), showWarnings = FALSE)
+            path        <- file.path(base.dir, parent.path, filename)
+            #
+            if (file.exists(path) && !force.recalc) {
+              if (show.message) {
+                cat(sprintf('Loading from cache (not calculating): %s\n', path))
+              }
+              load(path)
+            } else {
+              result <- fun(...)
+              if (show.message) {
+                cat(sprintf('Saving in cache: %s\n', path))
+              }
+              save(result, file = path)
+            }
+            return(result)
+          })
