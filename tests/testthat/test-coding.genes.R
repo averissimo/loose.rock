5a51f3d55422063d27ed48165c60cb5c6c4a2ef7
André Veríssimo <afsverissimo@gmail.com>
2018-07-24 06:40:31 +0100

    remove possible typos from description, and tried to speedup coding genes

--- tests/testthat/test-coding.genes.R
+++ tests/testthat/test-coding.genes.R
@@ -1,6 +1,9 @@
 context("coding.genes")
 
+loose.rock::show.message(FALSE)
+loose.rock::run.cache(coding.genes, verbose = FALSE)
+
 test_that("coding genes retrieves some genes", {
-  genes <- coding.genes(verbose = FALSE)
+  genes <- loose.rock::run.cache(coding.genes, verbose = FALSE)
   expect_true(all(c('BRCA1', 'BRCA2', 'CHADL', 'BTBD8', 'BCAS2', 'AGAP1') %in% genes$external_gene_name))
 })
